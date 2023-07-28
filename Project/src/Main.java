2aaecaee396a283c3bd28b7700abb33b460292f6
optimisme <optimisme@gmail.com>
2023-07-28 18:32:15 +0200

    Use 'data' folder

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +12,16 @@ public class Main {
 
    public static void main(String[] args) {
       
+        String basePath = System.getProperty("user.dir") + "/data/";
+
+        // Crear la carpeta 'data' si no existeix
+        File dir = new File(basePath);
+        if (!dir.exists()){
+            if(!dir.mkdirs()) {
+                System.out.println("Error en la creació de la carpeta 'data'");
+            }
+        }
+
       Manager.createSessionFactory();
 
       Cart refCart1 = Manager.addCart("Cart 1");
